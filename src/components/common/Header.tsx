26a6024abe314b8c2297f0f82bc7ae689ec72c38
Akshay Sharma <sharma.akshay5@gmail.com>
2022-02-21 13:28:13 +0530

    Minor changes

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,6 +4,6 @@ import Images from '../../res/Images';
 // import '../../App.css';
 export default function Header(props: HeaderProps) {
 	return (
-		<div className={'Title'}><img src={Images.logo.source} style={Images.logo.style}></img></div>
+		<div className={'Title'}><img alt-text={"LOGO"} src={Images.logo.source} style={Images.logo.style}></img></div>
 	);
 }
