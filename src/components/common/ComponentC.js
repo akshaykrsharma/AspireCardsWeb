b79a68fc2199504b493fb6a34bb38cd1611e185a
Akshay Sharma <sharma.akshay5@gmail.com>
2022-03-23 21:38:33 +0530

    - useContext at the place of consumer to get value from provider.

--- src/components/common/ComponentC.js
+++ src/components/common/ComponentC.js
@@ -1,16 +1,25 @@
-import React, { createContext } from 'react';
+import React, { useContext } from 'react';
 
 import { NameContext } from '../../App';
+
 export default function ComponentC() {
+	const user = useContext(NameContext);
 	return (
-		<NameContext.Consumer>
-			{value => {
-				return (
-					<div>
-						<h1>{value}</h1>
-					</div>
-				);
-			}}
-		</NameContext.Consumer>
+		<div>
+			<h1>{user}</h1>
+		</div>
 	);
+
+	//We can also use above short statement using useContext to get value of provider directly
+	// return (
+	// 	<NameContext.Consumer>
+	// 		{value => {
+	// 			return (
+	// 				<div>
+	// 					<h1>{value}</h1>
+	// 				</div>
+	// 			);
+	// 		}}
+	// 	</NameContext.Consumer>
+	// );
 }
